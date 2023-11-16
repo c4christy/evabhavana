8c16b8a0adaac4943a8f0fcac729b71652126473
c4christy <92540711+c4christy@users.noreply.github.com>
2023-11-16 19:32:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,13 +1,12 @@
 class script(object):
     START_TXT = """𝖧i {}, <b>𝖭𝗂𝖼𝖾 𝗍𝗈 𝗆𝖾𝖾𝗍 𝗒𝗈𝗎 🙌</b>
-ɪ'ᴍ ᴊᴜsᴛ ᴀ sɪᴍᴘʟᴇ ᴘʀᴇ-ғᴜɴᴄᴛɪᴏɴᴇᴅ ᴀᴜᴛᴏғɪʟᴛᴇʀ ʙᴏᴛ.
+ɪ'ᴍ ᴊᴜsᴛ ᴀ sɪᴍᴘʟᴇ ᴘʀᴇ-ғᴜɴᴄᴛɪᴏɴᴇᴅ ʙᴏᴛ.
 ɪᴛs ᴇᴀsʏ ᴛᴏ ᴜsᴇ ᴍᴇ ; ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀs ᴀᴅᴍɪɴ"""
 
-    ABOUT_TXT = """<b>‣ My Name : {}
-‣ Dᴇᴠᴏʟᴏᴘᴇʀ: <a href='https://t.me/TGxIRFAN'>𝐀𝐒 𝐓𝐆</a>
-‣ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a>
-‣ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a>
-‣ DᴀᴛᴀBᴀsᴇ : <a href='https://www.mongodb.com/'>MᴏɴɢᴏDB</a>"""
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/filesharebotusers>AML UPDATES</a>
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙿𝚈𝚃𝙷𝙾𝙽 7
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽: BHAVANA 𝚟3.0.0"""
 
 
     STATUS_TXT = """<b>➥ ᴛᴏᴛᴀʟ ғɪʟᴇs: <code>{}</code>
@@ -34,7 +33,6 @@ Nᴀᴍᴇ - {}"""
 🏷 𝐓𝐢𝐭𝐥𝐞 : {title}
 🎭 𝐆𝐞𝐧𝐫𝐞𝐬 : {genres}
 🌟 𝐑𝐚𝐭𝐢𝐧𝐠 : {rating}
-🎛 𝐂𝐨𝐮𝐧𝐭𝐫𝐢𝐞𝐬 : {countries}
 
 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 {message.chat.title}</b>"""
 
